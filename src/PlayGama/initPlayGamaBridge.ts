a40990c8bb69a930348dd10603d3bcce6483d971
Andrei Senaev <gsacryfice@gmail.com>
2025-05-08 07:57:33 +0000

    default message

--- src/PlayGama/initPlayGamaBridge.ts
+++ src/PlayGama/initPlayGamaBridge.ts
@@ -1,6 +1,75 @@
+type PlayGamaGame = {
+    appID: unknown;
+    title: unknown;
+    url: unknown;
+    coverURL: unknown;
+    iconURL: unknown;
+    isAvailable: unknown;
+};
+
 type PlayGamaBridge = {
     initialize: () => Promise<void>;
+    platform: {
+        id:
+        | 'playgama'
+        | 'vk'
+        | 'ok'
+        | 'yandex'
+        | 'facebook'
+        | 'crazy_games'
+        | 'game_distribution'
+        | 'wortal'
+        | 'playdeck'
+        | 'telegram'
+        | 'y8'
+        | 'lagged'
+        | 'msn'
+        | 'poki'
+        | 'qa_tool'
+        | 'mock';
+        sdk: null;
+        language: 'en' | 'ru';
+        payload: null;
+        tld: null;
+        isGetAllGamesSupported: boolean;
+        isGetGameByIdSupported: boolean;
+        getAllGames: () => Promise<PlayGamaGame[]>;
+        getGameById: (options: {
+            gameId: string;
+        }) => Promise<PlayGamaGame>;
+        sendMessage: (message: PlayGamaMessage) => void;
+        getServerTime: () => Promise<number>;
+    };
 };
+
+type PlayGamaMessages = {
+    /**
+     * The game has loaded, all loading screens have passed, the player can interact with the game.
+     */
+    game_ready: void;
+    /**
+     * Any loading within the game has started. For example, when a level is loading.
+     */
+    in_game_loading_started: void;
+    /**
+     * In-game loading has finished.
+     */
+    in_game_loading_stopped: void;
+    /**
+     * Gameplay has started. For example, the player has entered a level from the main menu.
+     */
+    gameplay_started: void;
+    /**
+     * Gameplay has ended/paused. For example, when exiting a level to the main menu, opening the pause menu, etc.
+     */
+    gameplay_stopped: void;
+    /**
+     * The player reached a significant moment. For example, defeating a boss, setting a record, etc.
+     */
+    player_got_achievement: void;
+};
+
+type PlayGamaMessage = keyof PlayGamaMessages;
 
 declare const bridge: PlayGamaBridge;
 
@@ -10,6 +79,14 @@ export function initPlayGamaBridge() {
         .then(() => {
             // eslint-disable-next-line no-console
             console.log('PlayGamaBridge initialized');
+
+            bridge.platform.getAllGames().then((games) => {
+                // eslint-disable-next-line no-console
+                console.log('Games', games);
+            }).catch((error) => {
+                // eslint-disable-next-line no-console
+                console.error('PlayGamaBridge getAllGames failed', error);
+            });
         })
         .catch((error) => {
             // eslint-disable-next-line no-console

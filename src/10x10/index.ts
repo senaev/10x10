124a9231da67ee2e66bd7c8b326f4ecc67338c1c
Andrei Senaev <gsacryfice@gmail.com>
2025-05-14 07:56:50 +0000

    default message

--- src/10x10/index.ts
+++ src/10x10/index.ts
@@ -1,19 +1,17 @@
 import 'jquery.transit';
 import { MILLISECONDS_IN_MINUTE, MILLISECONDS_IN_SECOND } from 'senaev-utils/src/types/Time/const';
-import { isPositiveInteger } from 'senaev-utils/src/utils/Number/PositiveInteger';
-import { isObject } from 'senaev-utils/src/utils/Object/isObject/isObject';
-import { isNonEmptyString } from 'senaev-utils/src/utils/String/NonEmptyString/NonEmptyString';
-import { decryptString, encryptString } from 'senaev-utils/src/utils/encryptDecryptString/encryptDecryptString';
 import { promiseTimeout } from 'senaev-utils/src/utils/timers/promiseTimeout/promiseTimeout';
 
 import { initPlayGamaBridge, PlayGamaBridge } from '../PlayGama/initPlayGamaBridge';
 import { GlobalThis } from '../types/GlobalThis';
 import { hintWebpackBuildTime } from '../utils/hintWebpackBuildTime';
 
-import { SESSION_ENCRYPTION_SECRET } from './const/SESSION_ENCRYPTION_SECRET';
 import { STORAGE_KEY } from './const/STORAGE_KEY';
-import { TenOnTen, TenOnTenState } from './js/TenOnTen';
+import { TenOnTen } from './js/TenOnTen';
 import './main.css';
+import {
+    encryptTenOnTenState, loadSavedTenOnTenInitialState,
+} from './utils/loadSavedTenOnTenInitialState';
 
 hintWebpackBuildTime();
 
@@ -22,20 +20,6 @@ const container = document.getElementById('app');
 if (!container) {
     throw new Error('Container not found');
 }
-
-const encryptState = (state: TenOnTenState): Promise<string> => {
-    const stateString = JSON.stringify(state);
-
-    return encryptString({
-        string: stateString,
-        secret: SESSION_ENCRYPTION_SECRET,
-    });
-};
-
-const decryptState = async (string: string): Promise<string> => decryptString({
-    string,
-    secret: SESSION_ENCRYPTION_SECRET,
-});
 
 let lastShownInterstitialTime = 0;
 const MIN_TIME_BETWEEN_ADS = MILLISECONDS_IN_MINUTE;
@@ -59,28 +43,12 @@ function showInterstitial(playGamaBridge: PlayGamaBridge) {
     playGamaBridge.advertisement.showInterstitial();
 }
 
-function isValidTenOnTenState(state: unknown): state is TenOnTenState {
-    return isObject(state) && isPositiveInteger(state.level) && isObject(state.current);
-}
-
 (async () => {
     const playGamaBridge = await initPlayGamaBridge();
 
     const isMockEnv = playGamaBridge.platform.id === 'mock';
 
-    const [encryptedState] = await playGamaBridge.storage.get([STORAGE_KEY]);
-    let initialState: TenOnTenState | undefined;
-    if (isNonEmptyString(encryptedState)) {
-        const decryptedState = await decryptState(encryptedState);
-
-        if (isNonEmptyString(decryptedState)) {
-            const stateObject = JSON.parse(decryptedState);
-
-            if (isValidTenOnTenState(stateObject)) {
-                initialState = stateObject;
-            }
-        }
-    }
+    const initialState = await loadSavedTenOnTenInitialState({ playGamaBridge });
 
     // eslint-disable-next-line no-console
     console.log('initialState', isMockEnv ? initialState : Boolean(initialState));
@@ -95,7 +63,7 @@ function isValidTenOnTenState(state: unknown): state is TenOnTenState {
     }).tenOnTen = tenOnTen;
 
     const saveState = async () => {
-        const encryptedStateNext: string = await encryptState(tenOnTen.getState());
+        const encryptedStateNext: string = await encryptTenOnTenState(tenOnTen.getState());
         playGamaBridge.storage.set([STORAGE_KEY], [encryptedStateNext]);
 
     };
---
+++ src/10x10/utils/loadSavedTenOnTenInitialState.ts
@@ -0,0 +1,81 @@
+import { decryptString, encryptString } from 'senaev-utils/src/utils/encryptDecryptString/encryptDecryptString';
+import { isPositiveInteger } from 'senaev-utils/src/utils/Number/PositiveInteger';
+import { isObject } from 'senaev-utils/src/utils/Object/isObject/isObject';
+import { isNonEmptyString } from 'senaev-utils/src/utils/String/NonEmptyString/NonEmptyString';
+
+import { PlayGamaBridge } from '../../PlayGama/initPlayGamaBridge';
+import { SESSION_ENCRYPTION_SECRET } from '../const/SESSION_ENCRYPTION_SECRET';
+import { STORAGE_KEY } from '../const/STORAGE_KEY';
+import { TenOnTenState } from '../js/TenOnTen';
+
+export const encryptTenOnTenState = (state: TenOnTenState): Promise<string> => {
+    const stateString = JSON.stringify(state);
+
+    return encryptString({
+        string: stateString,
+        secret: SESSION_ENCRYPTION_SECRET,
+    });
+};
+
+export const decryptTenOnTenState = async (string: string): Promise<string> => decryptString({
+    string,
+    secret: SESSION_ENCRYPTION_SECRET,
+});
+
+export function isValidTenOnTenState(state: unknown): state is TenOnTenState {
+    if (!isObject(state)) {
+        return false;
+    }
+
+    if (!isPositiveInteger(state.level)) {
+        return false;
+    }
+
+    if (!isObject(state.current)) {
+        return false;
+    }
+
+    return true;
+}
+
+const PERSIST_STATE_FOR_DEV_KEY = 'persist_state_for_dev';
+
+export async function loadSavedTenOnTenInitialState({
+    playGamaBridge,
+}: {
+    playGamaBridge: PlayGamaBridge;
+}): Promise<TenOnTenState | undefined> {
+    const isMockEnv = playGamaBridge.platform.id === 'mock';
+
+    const localStorageState = localStorage.getItem(PERSIST_STATE_FOR_DEV_KEY);
+
+    if (isMockEnv && isNonEmptyString(localStorageState)) {
+        const stateObject = JSON.parse(localStorageState);
+
+        if (!isValidTenOnTenState(stateObject)) {
+            throw new Error(`Invalid state from localStorage[${PERSIST_STATE_FOR_DEV_KEY}]`);
+        }
+
+        // eslint-disable-next-line no-console
+        console.warn(`loadSavedTenOnTenInitialState: using mock state from localStorage[${PERSIST_STATE_FOR_DEV_KEY}]`);
+
+        return stateObject;
+    }
+
+    const [encryptedState] = await playGamaBridge.storage.get([STORAGE_KEY]);
+
+    let initialState: TenOnTenState | undefined;
+    if (isNonEmptyString(encryptedState)) {
+        const decryptedState = await decryptTenOnTenState(encryptedState);
+
+        if (isNonEmptyString(decryptedState)) {
+            const stateObject = JSON.parse(decryptedState);
+
+            if (isValidTenOnTenState(stateObject)) {
+                initialState = stateObject;
+            }
+        }
+    }
+
+    return initialState;
+}
